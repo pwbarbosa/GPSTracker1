4328bbda3d0e4337cc1b24977431d46e7e18bed6
Pedro <pwbarbosa@gmail.com>
2023-03-09 20:28:01 -0500

    Initial commit

---
+++ BackEnd/src/main/java/com/rossinicoder/GPSTracker1/GpsTracker1Application.java
@@ -0,0 +1,13 @@
+package com.rossinicoder.GPSTracker1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsTracker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsTracker1Application.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/rossinicoder/GPSTracker1/GpsTracker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rossinicoder.GPSTracker1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsTracker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
